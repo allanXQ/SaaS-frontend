5dba47e04437b97f7ef9b44ccb898054dcbb38e6
Allan Juma <allan.juma@savannahinformatics.com>
2025-12-14 21:08:34 +0300

    chore: disable linting

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
+module.exports = {
+  ...nextConfig,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+};
